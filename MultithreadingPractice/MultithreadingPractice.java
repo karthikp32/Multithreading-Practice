3024ce5c54c2c35e74808a6de5c018d3073f1df1
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2023-09-07 19:04:27 +0000

    resolved bug by changing inner class PracticeThread to a static class so it can be accessed without creating an instance of the outer class

--- MultithreadingPractice/MultithreadingPractice.java
+++ MultithreadingPractice/MultithreadingPractice.java
@@ -1,7 +1,5 @@
 package MultithreadingPractice;
 
-import ExtendingThread.PracticeThread;
-
 public class MultithreadingPractice {
 
 // Problem statement: You need to run the below code with at least three threads (name those threads as A,B,C....), 
@@ -26,9 +24,38 @@ public class MultithreadingPractice {
     //     }
     // }
 
+    public static class PracticeThread extends Thread {
+
+        String name = "";
+
+        public PracticeThread() {
+
+        }
+
+        public PracticeThread(String name) {
+            this.name = name;
+        }
+    
+        public void run() {
+            System.out.println("Thread " + this.name + "is being executed by one of the cores of the CPU.");
+            int sum = 0;
+            for (int i=1; i <10; i++) {
+                sum += i;
+                System.out.println("Thread : " + this.name + " - value : " + sum);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread was sleeping for 2s and got interrupted.");
+                }
+            
+            }
+        }
+    }
+
     public static void main(String[] args) {
         for (int i=0; i < 3; i++) {
-            String threadName = "" + (char) (i + 65);
+            // String threadName = "" + (char) (i + 65);
+            String threadName = "" + i;
             PracticeThread currThread = new PracticeThread(threadName);
             currThread.start();
         }
